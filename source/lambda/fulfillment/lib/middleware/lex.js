0c2817bedca6c3469116109cd483e0213de029e5
bryarch-omes <125496331+bryarch-omes@users.noreply.github.com>
2025-07-17 18:50:16 -0500

    Update lex.js
    Implementing the fullfilment fork for the CXOne implementation.

--- source/lambda/fulfillment/lib/middleware/lex.js
+++ source/lambda/fulfillment/lib/middleware/lex.js
@@ -1,7 +1,15 @@
-/** ************************************************************************************************
-*   Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.                             *
-*   SPDX-License-Identifier: Apache-2.0                                                            *
- ************************************************************************************************ */
+/*********************************************************************************************************************
+ *  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.                                                *
+ *                                                                                                                    *
+ *  Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance    *
+ *  with the License. A copy of the License is located at                                                             *
+ *                                                                                                                    *
+ *      http://www.apache.org/licenses/                                                                               *
+ *                                                                                                                    *
+ *  or in the 'license' file accompanying this file. This file is distributed on an 'AS IS' BASIS, WITHOUT WARRANTIES *
+ *  OR CONDITIONS OF ANY KIND, express or implied. See the License for the specific language governing permissions    *
+ *  and limitations under the License.                                                                                *
+ *********************************************************************************************************************/
 
 const _ = require('lodash');
 const slackifyMarkdown = require('slackify-markdown');
@@ -371,6 +379,36 @@ function getV2DialogCodeHookResponseTemplate(request, response) {
     };
 }
 
+function getV2CXoneTemplate(request, response) {
+    const messages = [
+        {
+            contentType: response.type,
+            content: response.message,
+        }
+    ];
+
+    if (_.get(response, 'session.customPayload')) {
+        messages.push({
+            contentType: 'CustomPayload',
+            content: _.get(response, 'session.customPayload', 'Empty')
+        });
+    }
+
+    return {
+        sessionState: {
+            sessionAttributes: _.get(response, 'session', {}),
+            dialogAction: {
+                type: 'Close',
+            },
+            intent: {
+                name: response.intentname,
+                state: 'Fulfilled',
+            },
+        },
+        messages: messages,
+    };
+}
+
 function assembleLexV2Response(request, response) {
     let out = {};
 
@@ -382,7 +420,8 @@ function assembleLexV2Response(request, response) {
     } else if (_.get(request, 'invocationSource') === 'DialogCodeHook') {
         out = getV2DialogCodeHookResponseTemplate(request, response);
     } else {
-        out = getV2CloseTemplate(request, response);
+        //out = getV2CloseTemplate(request, response);
+        out = getV2CXoneTemplate(request, response);
     }
 
     if (!isConnectClient(request)) {
